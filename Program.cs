6b72e1b63753486507ca9fe108d80e9e929044b4
unknown <abhisheksrivastava9719@gmail.com>
2022-07-13 06:40:26 +0530

    [Abhishek] Added Analyze and respond function with happy and sad test cases

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿using MoodAnalyzer;
 Console.WriteLine("Welcome to the mood analyzer");
+moodAnalyzer obj = new moodAnalyzer();
+Console.WriteLine(obj.AnalyzeMood("I am in a Sad mood"));
---
+++ moodAnalyzer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    public class moodAnalyzer
+    {
+        public string AnalyzeMood(String mood)
+        {
+            if (find(mood))
+            {
+                return "happy";
+            }
+            else
+            {
+                return "sad";
+            }
+        }
+        public bool find(string findMood)
+        {
+            string[] ar = findMood.Split(' ');
+            for (int x = 0; x < ar.Length; x++)
+            {
+                Console.WriteLine(ar[x]);
+                ar[x]=ar[x].ToLower();
+                Console.WriteLine(ar[x]);
+                if (ar[x].Equals("sad"))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
+
